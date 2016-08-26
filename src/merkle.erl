a197838ad6d08aae845339c26ee930baedb8ef92
Carlos Galdino <carloshsgaldino@gmail.com>
2016-08-26 10:41:47 -0300

    Simplify tree structure.

--- src/merkle.erl
+++ src/merkle.erl
@@ -2,18 +2,16 @@
 
 -define(HASH, sha256).
 
--record(leaf, {key :: key(),
-               hash :: hash()}).
--record(inner, {hash :: hash(),
+-record(inner, {key :: key(),
+                hash :: hash(),
+                height :: non_neg_integer(),
                 min_key :: key(),
                 max_key :: key(),
-                left :: non_empty_tree(),
-                right :: non_empty_tree()}).
+                left :: tree(),
+                right :: tree()}).
 
--type leaf() :: #leaf{}.
 -type inner() :: #inner{}.
--type non_empty_tree() :: leaf() | inner().
--type tree() :: non_empty_tree() | 'nil'.
+-type tree() :: inner() | 'nil'.
 
 -type key() :: binary().
 -type value() :: binary().
@@ -35,7 +33,7 @@ build_raw(L) ->
 
 -spec build([{key(), value()}]) -> tree().
 build(L) ->
-    List = [to_leaf(X) || X <- lists:keysort(1, L)],
+    List = [to_inner(X) || X <- lists:keysort(1, L)],
     build_tree(List).
 
 -spec diff(tree(), tree()) -> [key()].
@@ -55,7 +53,7 @@ build_tree(List) ->
     UpperLevel = lists:reverse(combine(List, [])),
     build_tree(UpperLevel).
 
--spec combine([non_empty_tree()], [non_empty_tree()]) -> [non_empty_tree()].
+-spec combine([tree()], [tree()]) -> [tree()].
 combine([], Acc) ->
     Acc;
 combine([X], Acc) ->
@@ -63,22 +61,17 @@ combine([X], Acc) ->
 combine([X, Y | T], Acc) ->
     combine(T, [to_inner(X, Y) | Acc]).
 
--spec to_leaf({key(), value()}) -> leaf().
-to_leaf({Key, Value}) ->
-    #leaf{key = Key, hash = crypto:hash(?HASH, <<Key/binary, Value/binary>>)}.
+-spec to_inner({key(), value()}) -> inner().
+to_inner({Key, Value}) ->
+    #inner{key = Key, min_key = Key, max_key = Key, height = 0, left = 'nil', right = 'nil', hash = crypto:hash(?HASH, <<Key/binary, Value/binary>>)}.
 
--spec to_inner(non_empty_tree(), non_empty_tree()) -> inner().
-to_inner(L = #leaf{hash = LHash, key = LKey}, R = #leaf{hash = RHash, key = RKey}) ->
-    #inner{left = L, right = R, min_key = LKey, max_key = RKey, hash = crypto:hash(?HASH, <<LHash/binary, RHash/binary>>)};
-to_inner(L = #inner{hash = LHash, min_key = MinKey}, R = #leaf{hash = RHash, key = RKey}) ->
-    #inner{left = L, right = R, min_key = MinKey, max_key = RKey, hash = crypto:hash(?HASH, <<LHash/binary, RHash/binary>>)};
-to_inner(L = #leaf{hash = LHash, key = LKey}, R = #inner{hash = RHash, max_key = MaxKey}) ->
-    #inner{left = L, right = R, min_key = LKey, max_key = MaxKey, hash = crypto:hash(?HASH, <<LHash/binary, RHash/binary>>)};
-to_inner(L = #inner{hash = LHash, min_key = MinKey}, R = #inner{hash = RHash, max_key = MaxKey}) ->
-    #inner{left = L, right = R, min_key = MinKey, max_key = MaxKey, hash = crypto:hash(?HASH, <<LHash/binary, RHash/binary>>)}.
+-spec to_inner(tree(), tree()) -> inner().
+to_inner(L = #inner{hash = LHash, min_key = MinKey, height = LHeight}, R = #inner{hash = RHash, max_key = MaxKey, height = RHeight}) ->
+    Height = max(LHeight, RHeight) + 1,
+    #inner{left = L, right = R, height = Height, min_key = MinKey, max_key = MaxKey, hash = crypto:hash(?HASH, <<LHash/binary, RHash/binary>>)}.
 
--spec extract_keys(non_empty_tree()) -> [{key(), hash()}].
-extract_keys(#leaf{key = Key, hash = Hash}) ->
+-spec extract_keys(tree()) -> [{key(), hash()}].
+extract_keys(#inner{key = Key, hash = Hash, left = 'nil', right = 'nil'}) ->
     [{Key, Hash}];
 extract_keys(#inner{left = Left, right = Right}) ->
     lists:flatten([extract_keys(Left), extract_keys(Right)]).
@@ -93,23 +86,15 @@ remove_equal_elements([H|T], Set) ->
     end.
 
 -spec dirty_diff(tree(), tree()) -> [{key(), hash()}].
-dirty_diff('nil', 'nil') ->
+dirty_diff(T, T) ->
     [];
 dirty_diff('nil', T2) ->
     lists:flatten([extract_keys(T2)]);
 dirty_diff(T1, 'nil') ->
     lists:flatten([extract_keys(T1)]);
-dirty_diff(#leaf{hash = _Hash}, #leaf{hash = _Hash}) ->
-    [];
-dirty_diff(#inner{hash = _Hash}, #inner{hash = _Hash}) ->
-    [];
-dirty_diff(T1 = #leaf{hash = _Hash1}, T2 = #leaf{hash = _Hash2}) ->
-    [extract_keys(T1), extract_keys(T2)];
-dirty_diff(T1 = #inner{hash = _Hash1, min_key = MinKey1, max_key = MaxKey1}, T2 = #inner{hash = _Hash2, min_key = MinKey2, max_key = MaxKey2}) when MaxKey1 > MaxKey2, MinKey1 >= MinKey2 ->
+dirty_diff(T1 = #inner{hash = _Hash1, height = LHeight}, T2 = #inner{hash = _Hash2, height = RHeight}) when LHeight > RHeight ->
     dirty_diff(T2, T1);
-dirty_diff(Tree = #inner{}, Leaf = #leaf{}) ->
-    dirty_diff(Leaf, Tree);
-dirty_diff(Leaf = #leaf{}, Tree = #inner{}) ->
+dirty_diff(Leaf = #inner{left = 'nil', right = 'nil'}, Tree = #inner{}) ->
     Diff = case contained_branch(Leaf, Tree) of
                left -> [dirty_diff(Leaf, Tree#inner.left), extract_keys(Tree#inner.right)];
                right -> [dirty_diff(Leaf, Tree#inner.right), extract_keys(Tree#inner.left)];
@@ -124,27 +109,20 @@ dirty_diff(T1 = #inner{hash = _Hash1}, T2 = #inner{hash = _Hash2}) ->
            end,
     lists:flatten(Diff).
 
--spec contains(non_empty_tree(), non_empty_tree()) -> boolean().
-contains(#leaf{key = Key}, #inner{min_key = MinKey}) ->
-    MinKey > Key;
+-spec contains(tree(), tree()) -> boolean().
+contains(#inner{key = Key, left = 'nil', right = 'nil'}, #inner{min_key = MinKey}) ->
+    MinKey >= Key;
 contains(#inner{min_key = MinKey1, max_key = MaxKey1}, #inner{min_key = MinKey2, max_key = MaxKey2}) when MinKey1 >= MinKey2, MaxKey1 =< MaxKey2 ->
     true;
 contains(_T1, _T2) ->
     false.
 
--spec contained_branch(non_empty_tree(), non_empty_tree()) -> 'left' | 'right' | 'none'.
-contained_branch(#leaf{key = Key}, #inner{left = #leaf{key = Key}}) ->
-    left;
-contained_branch(#leaf{key = Key}, #inner{right = #leaf{key = Key}}) ->
-    right;
-contained_branch(#leaf{}, #inner{left = #leaf{}, right = #leaf{}}) ->
+-spec contained_branch(tree(), tree()) -> 'left' | 'right' | 'none'.
+contained_branch('nil', _) ->
     none;
-contained_branch(Leaf = #leaf{}, #inner{left = Left = #inner{}, right = Right = #inner{}}) ->
-    case contains(Leaf, Right) of
-        false -> contained_branch(Leaf, Left);
-        true -> contained_branch(Leaf, Right)
-    end;
-contained_branch(#inner{max_key = MaxKey1}, #inner{min_key = MinKey2}) when MaxKey1 =< MinKey2 ->
+contained_branch(_, 'nil') ->
+    none;
+contained_branch(#inner{max_key = MaxKey1, left = #inner{}, right = #inner{}}, #inner{min_key = MinKey2}) when MaxKey1 =< MinKey2 ->
     none;
 contained_branch(T1 = #inner{}, #inner{left = Left2, right = Right2}) ->
     case contains(T1, Left2) of
